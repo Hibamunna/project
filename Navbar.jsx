e5430e0ea09bbd79a6dc2fcde39d1c15399f15b0
Hibamunna <165657440+Hibamunna@users.noreply.github.com>
2024-04-16 10:01:04 +0530

    Add files via upload

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,23 +1,59 @@
-import { AppBar, Button, Toolbar, Typography } from '@mui/material'
+import { AppBar, Button,Toolbar, Typography } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import IconButton from '@mui/material/IconButton';
+// import DeleteIcon from '@mui/icons-material/Delete';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import Image from './src/Image/resto.jpg';
 
 const Navbar = () => {
+  const backgroundImageStyle = {
+    position:'fixed',
+    top:0,
+    left:0,
+    width:'100%',
+    height:'100%',
+    zIndex:-1,
+  };
+
   return (
     <div>
-          <AppBar>
-<Toolbar>
-<Typography>Employee App</Typography> &nbsp; &nbsp;
-<Button variant='contained' color='success'>
-  <Link to={'/'} style={{textDecoration:'none', color:'white'}}>View</Link>
-    </Button>
-    &nbsp; &nbsp;
-<Button variant='contained' color='error'>
-  <Link to={'/add'} style={{textDecoration:'none', color:'white'}}>Add </Link>
-    </Button>
-</Toolbar>
-</AppBar>
-    </div>
+      <img
+       src={Image}
+       alt="background"
+       style={backgroundImageStyle }/>
+       </div>
   )
+  <div>
+      <AppBar color='error'>
+        <Toolbar>
+            <Typography variant='h5'>RED STILLETTO RESTURANT</Typography>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <div>
+            <Button color='error'>
+            <Link to={'/Homepage'} style={{textDecoration:'none', color:'white'}}>HOME</Link>
+            </Button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <Button>
+               <Link to={''} style={{textDecoration:'none', color:'white'}}>MENU</Link>
+            </Button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <Button>
+            <Link to={''} style={{textDecoration:'none', color:'white'}}>OFFERS</Link>
+            </Button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <Button>
+
+            <Link to={'/Login'} style={{textDecoration:'none', color:'white'}}>LOGIN</Link>
+            </Button>&nbsp;&nbsp;
+            <Button>
+            <Link to={'/Signup'} style={{textDecoration:'none', color:'white'}}>SIGNUP</Link>
+            </Button>
+          
+           
+            <IconButton aria-label="Dashboard" color='secondary'>
+        <DashboardIcon />
+      </IconButton>
+            </div>
+        </Toolbar>
+      </AppBar>
+    </div>
 }
+
 export default Navbar
